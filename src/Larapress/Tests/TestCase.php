5c39aa471a05ac3e5242a37dc176500dc8996c7c
Martin Hettiger <Martin@Hettiger.com>
2014-05-18 18:10:41 +0200

    Update the TestCase

--- src/Larapress/Tests/TestCase.php
+++ src/Larapress/Tests/TestCase.php
@@ -20,7 +20,7 @@ class TestCase extends LaravelTestCase
 
         $testEnvironment = 'testing';
 
-        return require __DIR__ . '/../../../bootstrap/start.php';
+        return require __DIR__ . '/../../../../../../bootstrap/start.php';
     }
 
     protected function tearDown()
